78b7f4a8964fc67b0887cf09544cdd6a585b564d
Naumov Konstantin <constantin.naumov@gmail.com>
2020-10-30 20:51:37 +0300

    Handle queue message

--- src/MessageHandler/CreateUserHandler.php
+++ src/MessageHandler/CreateUserHandler.php
@@ -3,6 +3,7 @@
 namespace App\MessageHandler;
 
 use App\Message\CreateUser;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 /**
@@ -11,14 +12,40 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
  */
 final class CreateUserHandler implements MessageHandlerInterface
 {
+
+    /**
+     * Subject of message
+     * @var string
+     */
+    public const SUBJECT = 'Регистрация нового пользователя';
+
+    protected $mailer;
+
+    protected $logger;
+
+    public function __construct(\Swift_Mailer $mailer, LoggerInterface $logger)
+    {
+        $this->mailer = $mailer;
+        $this->logger = $logger;
+    }
+
     /**
      * Process the message
      *
-     * @param CreateUser $message
+     * @param CreateUser $event_message
      * @return void
      */
-    public function __invoke(CreateUser $message): void
+    public function __invoke(CreateUser $event_message): void
     {
-        
+        $user = $event_message->getUser();
+
+        $message = new \Swift_Message();
+        $message->setSubject(self::SUBJECT)
+            ->setTo($user->getEmail())
+            ->setBody('test123');
+
+        $this->mailer->send($message);
+
+        $this->logger->info('CreateUser. Email sent to ' . $user->getEmail());
     }
 }
